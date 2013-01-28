7bbb230f449aeb2b92319398dcf9df2b22946838
Greg Roodt <groodt@gmail.com>
2013-01-28 18:04:17 +0000

    Fix sequence fn.

--- solutions/6-higher-order-functions.hs
+++ solutions/6-higher-order-functions.hs
@@ -1,8 +1,9 @@
--- Sum the numbers between two values recursively, assuming a < b when the function is first called
--- Inclusive or exclusive?
+-- Sum the numbers between two inclusive values recursively, assuming a < b when the function is first called
+-- Example: sumInts 0 1 = 1
+--          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
 sumInts a b
-        | a == b = 0
+        | a > b = 0
         | otherwise = a + sumInts (a + 1) b
 
 -- Define a square function
@@ -12,7 +13,7 @@ sq x = x * x
 -- Sum the squares between two numbers. This function should be similar to the sumInts function
 sumSquares :: Int -> Int -> Int
 sumSquares a b
-           | a == b = 0
+           | a > b = 0
            | otherwise = sq a + sumSquares (a + 1) b
 
 -- Define a higher order sum function which accepts an (Int -> Int) function to apply to all integers between two values.
@@ -32,13 +33,21 @@ hoSumSquares a b = higherOrderSum sq a b
 hoSumInts :: Int -> Int -> Int
 hoSumInts = (\a b -> higherOrderSum id a b)
 
--- Create a new higher order method which generalises over the function provided by sum (That is, (+) :: Int -> Int -> Int)
+-- Create a new higher order method which generalises over the function provided by sumInts (That is, (+) :: Int -> Int -> Int) between a and b
+-- This will give the ability to perform utilities such as the prodcut of all squares (or any other Int -> Int function) between a and b
 -- You will also need to generalise the base case
 -- You can also define the function signature yourself, which leaves you free to define the parameters and their order
--- Not sure what the question means. Does it mean redefine plus? Or define f such that f (+) 0 xs == sum xs ?
-higherOrderSequenceApplication binaryF start xs = foldr binaryF start xs
+-- To be clear, your function will need to handle:
+--  - A start value, a :: Int
+--  - A end value, b :: Int
+--  - A function to apply to each value, op :: Int -> Int
+--  - A function to apply between each value, f :: Int -> Int -> Int
+--  - A value to return in the base case when a > b, z :: Int
+higherOrderSequenceApplication start end mapFn reduceFn zero
+                               | start > end = zero
+                               | otherwise = reduceFn (mapFn start) $ mapFn (higherOrderSequenceApplication (start + 1) end mapFn reduceFn zero)
 
 -- Define a factorial method using the higherOrderSequenceAppliction
 hoFactorial :: Int -> Int
-hoFactorial x = higherOrderSequenceApplication (*) 1 [1..x]
+hoFactorial x = higherOrderSequenceApplication 1 x id (*) 1
 
