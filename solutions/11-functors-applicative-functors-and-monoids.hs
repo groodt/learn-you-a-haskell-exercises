811eb9556b4858dfdd268490ffd84157654415bd
Greg Roodt <groodt@gmail.com>
2013-02-20 22:25:07 +0000

    Ensure List instance meets laws for Applicative Functor.

--- solutions/11-functors-applicative-functors-and-monoids.hs
+++ solutions/11-functors-applicative-functors-and-monoids.hs
@@ -34,11 +34,27 @@ instance Applicative List where
 -- Make sure that the List obeys the laws for Applicative and Monoid
 
 -- Applicative laws
+
 -- pure f <*> x = fmap f x
+-- pure id <*> v = v
+-- pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+-- pure f <*> pure n = pure (f n)
+-- f1 <*> pure n = pure ($ n) <*> f1
 testApplicativeLaws = 
-                let rule1 = (pure (+3) <*> ((Value 1) ((Value 2) ((Value 3) Empty)))) == (fmap (+3) ((Value 1) ((Value 2) ((Value 3) Empty))))
-                    rule2 = (pure id <*> (Value 1 Empty)) == Value 1 Empty
-                in rule1 && rule2
+                let 
+                    f = (+3)
+                    x = Value 1 $ Value 2 $ Value 3 $ Value 4 $ Empty
+                    u = Value (+10) $ Empty
+                    v = Value (+5) Empty
+                    w = Value 3 $ Empty
+                    f1 = Value (+5) Empty
+                    n = 5
+                    rule1 = (pure f <*> x) == fmap f x
+                    rule2 = (pure id <*> x) == x
+                    rule3 = (pure (.) <*> u <*> v <*> w) == (u <*> (v <*> w))
+                    rule4 = (pure f <*> pure n) == (pure (f n) :: List Integer)
+                    rule5 = (f1 <*> pure n) == (pure ($ n) <*> f1)
+                in rule1 && rule2 && rule3 && rule4 && rule5
 
 
 -- Monoid laws
